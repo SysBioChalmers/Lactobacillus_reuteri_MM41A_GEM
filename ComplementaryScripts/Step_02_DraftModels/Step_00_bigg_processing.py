5c27ba49bfdc7c84bfcc112b343711745400bacc
lhao <lhao@chalmers.se>
2019-05-29 14:32:38 +0200

    refactor: bigg_rea/met_df saved as .csv file and easy to load (remove old files)

--- ComplementaryScripts/Step_02_DraftModels/Step_00_bigg_processing.py
+++ ComplementaryScripts/Step_02_DraftModels/Step_00_bigg_processing.py
@@ -25,26 +25,34 @@ def str_rea_metabolites(rea_metabolites):
     for k, v in rea_metabolites.items():
         temp_dic[k.id] = v
     str_mets = str(temp_dic)
-
     return str_mets
 
 
 os.chdir('../../ComplementaryData/bigg_database/')
+# %% < read bigg model>
 
+case = 'other' # 'first' or 'other'
+if case =='first':
+    # pockle bigg model
+    bigg_model = cobra.io.load_json_model('universal_model.json')
+    with open('universal_model.pickle', 'wb') as f:
+       pickle.dump(bigg_model, f)
+else:
+    with open('universal_model.pickle', 'rb') as f:
+        bigg_model = pickle.load(f)
 
-# pockle bigg model
-bigg_model = cobra.io.load_json_model('universal_model.json')
-with open('universal_model.pickle', 'wb') as f:
-   pickle.dump(bigg_model, f)
+# %% < build met report>
+bigg_met_df1 = pd.read_csv('bigg_database/bigg_models_metabolites.txt', sep='\t', usecols=[0, 1, 2, 5])
+bigg_met_df1.to_csv('bigg_met_df.csv',sep = '\t',index=False)
 
-
+# %% < build met report >
 # the section to build all bigg_rea_df (    bigg_rea_df.to_csv('../../../bigg_database/bigg_models_reactions_all.txt'))
 # becaues the reaction_strings in file are not irregular
-
 # bigg_rea_df from file
+#   option 1
 bigg_rea_df1 = pd.read_csv('bigg_models_reactions.txt', sep='\t', usecols=[0, 2, 5])
 
-# bigg_rea_df from model
+#   option 2 bigg_rea_df from model
 columns = ['bigg_id', 'equaction_string', 'mets', 'bounds', 'original_bigg_ids']
 bigg_rea_df2 = pd.DataFrame(columns=columns)
 
@@ -63,12 +71,9 @@ for rea in bigg_model.reactions:
     rowlist.append(original_bigg_ids)
 
     bigg_rea_df2.loc[len(bigg_rea_df2)] = rowlist
-bigg_rea_df2.to_csv('bigg_models_reactions_from_model.txt', index=False)
+#bigg_rea_df2.to_csv('bigg_models_reactions_from_model.txt', index=False)
 bigg_rea_df = pd.merge(bigg_rea_df1, bigg_rea_df2, how='outer', on='bigg_id')
-bigg_rea_df.to_csv('bigg_models_reactions_all.txt', index=False)
+bigg_rea_df.to_csv('bigg_rea_df.csv', sep = '\t', index=False)
 
 
-with open('universal_model.pickle', 'rb') as f:
-    bigg_model = pickle.load(f)
 
-bigg_rea_df = pd.read_csv('bigg_models_reactions_all.txt')
